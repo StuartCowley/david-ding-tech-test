33c63d6a4473fa48e4c8199f4067c80676cc550f
David Ding <dingtechxing16@gmail.com>
2023-06-22 01:46:44 +0100

    Modified getImages to filter out only images in the object

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import "../styles/search.css";
+import getImages from "../requests/getImages";
 
 const Search = () => {
   const [value, setValue] = useState();
   const handleInputChange = (e) => setValue(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getImages(value);
+  };
   return (
     <>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           type="text"
           className="search-input"
