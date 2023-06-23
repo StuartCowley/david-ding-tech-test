ef75a2a925fa2137d1ada27a4aba1f886634f44d
David Ding <dingtechxing16@gmail.com>
2023-06-23 14:34:25 +0100

    Fixed array error

--- src/_tests_/SeachResults.test.js
+++ src/_tests_/SeachResults.test.js
@@ -4,12 +4,14 @@ import SearchResults from "../components/SearchResults";
 
 describe("SearchResults", () => {
   it("renders correctly", () => {
-    const { asFragment } = render(<SearchResults />);
+    const results = [];
+    const { asFragment } = render(<SearchResults results={results} />);
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders the correct text", () => {
-    render(<SearchResults />);
+    const results = [];
+    render(<SearchResults results={results} />);
     expect(screen.getByText("No results!")).toBeInTheDocument();
   });
 });
