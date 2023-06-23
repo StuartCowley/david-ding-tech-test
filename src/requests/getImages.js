59db8e253eb9052173d7c2733f05ae75e653ad61
David Ding <dingtechxing16@gmail.com>
2023-06-23 15:00:23 +0100

    Updated GET request to improve specificity of searching

--- src/requests/getImages.js
+++ src/requests/getImages.js
@@ -5,14 +5,13 @@ const getImages = (query) => {
     return Promise.resolve([]);
   } else {
     return axios
-      .get(`https://images-api.nasa.gov/search?q=moon`)
+      .get(`https://images-api.nasa.gov/search?q=${query}`)
       .then((res) => {
         const imageResults = res.data.collection.items;
         const parsedImages = imageResults.filter(
           (obj) => obj.data[0].media_type === "image"
         );
         const images = parsedImages.map((image) => image.links[0].href);
-        // console.log(images);
         return images;
       })
       .catch((err) => {
